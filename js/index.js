59ba71cbb85388fa4c4784a4dfe338a1f7e837f0
Bret Banger <Banger484@gmail.com>
2021-09-28 00:00:39 -0500

    working through problems

--- js/index.js
+++ js/index.js
@@ -40,3 +40,63 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+//
+const Awesome = document.querySelector('.cta h1');
+Awesome.textContent = 'Dom is Awesome'
+const button = document.querySelector('.cta button');
+button.textContent = "Get Started"
+
+const topImg = document.querySelector('#cta-img');
+topImg.src = "img/header-img.png";
+
+const navLinks = document.querySelectorAll('a');
+navLinks[0].style.color = 'green';
+//
+
+const links = document.querySelectorAll('nav a');
+links[0].textContent = "Services";
+links[1].textContent = "Product";
+links[2].textContent = "Vision";
+links[3].textContent = "Features";
+links[4].textContent = "About";
+links[5].textContent = "Contact";
+//
+
+const mainContent = document.querySelectorAll('.main-content h4');
+mainContent[0].textContent = "Features";
+mainContent[1].textContent = "About";
+mainContent[2].textContent = "Services";
+mainContent[3].textContent = "Product";
+mainContent[4].textContent = "Vision";
+
+const mainPars = document.querySelectorAll('.main-content p');
+mainPars[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainPars[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainPars[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainPars[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainPars[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+
+//
+const contact = document.querySelectorAll('.contact h4');
+contact[0].textContent = 'Contact'
+const contactInfo = document.querySelectorAll('.contact p');
+contactInfo[0].textContent = "123 Way 456 Street Somewhere, USA";
+contactInfo[1].textContent = "1 (888) 888-8888";
+contactInfo[2].textContent = "sales@greatidea.io";
+
+
+
+//
+
+ const middleImage = document.querySelector('#middle-img');
+ middleImage.src = 'img/mid-page-accent.jpg';
+ //
+const storeLink = document.createElement('a'); 
+storeLink.textContent = 'Store';
+storeLink.href = '#';
+document.querySelector('nav').appendChild(storeLink);
+const stuffLink = document.createElement('a');
+stuffLink.textContent = 'Stuff';
+stuffLink.href = '#';
+document.querySelector('nav').prepend(stuffLink);
